a85c1a89b5755b9875368c22a5b0dd452a2c38f0
yushayypbek <uristan92@gmail.com>
2022-12-15 19:55:30 +0600

    2.2

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,5 +1,4 @@
 package app.model;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -10,14 +9,15 @@ public class AnimalsCage {
     @Autowired
     @Qualifier("dog")
     private Animal animal;
-    @Autowired
 
+    @Autowired
     private Timer timer;
+
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 
--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -1,13 +1,11 @@
 package app.model;
-
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-
-public class Dog extends Animal {
-
+public class Dog extends Animal{
 
     @Override
-    public String toString() { return "I'm a dog" ;}
+    public String toString() {
+        return "Im a Dog";
+    }
 }
